009ad95024781fdd357179b0c5ea8425d77c13d1
곽태욱 <gwak2837@kakao.com>
2024-02-21 22:21:25 +0900

    Update KeyboardController.tsx

--- src/app/dialogue/KeyboardController.tsx
+++ src/app/dialogue/KeyboardController.tsx
@@ -1,29 +1,29 @@
 'use client'
 
 import KeybordShortcut from '@/components/KeybordShortcut'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 export default function SortProducts() {
   const searchParams = useSearchParams()
+  const router = useRouter()
+
+  function decreaseDialogId() {
+    const newParams = new URLSearchParams(searchParams.toString())
+    const dialogId = +(newParams.get('id') ?? 0)
+    newParams.set('id', String(dialogId > 0 ? dialogId - 1 : dialogId))
+    router.push(`?${newParams.toString()}`)
+  }
 
   function increaseDialogId() {
     const newParams = new URLSearchParams(searchParams.toString())
     const dialogId = +(newParams.get('id') ?? 0)
     newParams.set('id', String(dialogId + 1))
-    history.pushState(null, '', `?${newParams.toString()}`)
+    router.push(`?${newParams.toString()}`)
   }
 
   return (
     <>
-      <KeybordShortcut
-        keyCode="ArrowLeft"
-        onKeyDown={() => {
-          const newParams = new URLSearchParams(searchParams.toString())
-          const dialogId = +(newParams.get('id') ?? 0)
-          newParams.set('id', String(dialogId > 0 ? dialogId - 1 : dialogId))
-          history.pushState(null, '', `?${newParams.toString()}`)
-        }}
-      />
+      <KeybordShortcut keyCode="ArrowLeft" onKeyDown={decreaseDialogId} />
       <KeybordShortcut keyCode="ArrowRight" onKeyDown={increaseDialogId} />
       <KeybordShortcut keyCode="Enter" onKeyDown={increaseDialogId} />
     </>

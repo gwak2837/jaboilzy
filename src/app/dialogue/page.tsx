f15b14996ae8468b7f5b82db2178e68c37dfa003
곽태욱 <gwak2837@kakao.com>
2024-02-21 20:46:46 +0900

    Update page.tsx

--- src/app/dialogue/page.tsx
+++ src/app/dialogue/page.tsx
@@ -1,11 +1,17 @@
+import { Suspense } from 'react'
+
 import DialogueId from './DialogueId'
 import SortProducts from './KeyboardController'
 
 export default function Page({ searchParams }: PageProps) {
   return (
     <div className="flex items-center justify-center h-[100svh]">
-      <DialogueId />
-      <SortProducts />
+      <Suspense fallback={<div>...</div>}>
+        <DialogueId />
+      </Suspense>
+      <Suspense fallback={null}>
+        <SortProducts />
+      </Suspense>
     </div>
   )
 }
